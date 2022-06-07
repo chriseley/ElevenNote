821f362898c653ed9978f47c31ca11bc39ea9d74
chriseley <crobineley@gmail.com>
2022-06-07 19:51:36 -0400

    .

--- ElevenNote.WebAPI/Startup.cs
+++ ElevenNote.WebAPI/Startup.cs
@@ -2,10 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 public class Startup {
+
+    public IConfiguration Configuration { get; }
     public Startup(IConfiguration configuration) {
         Configuration = configuration;
     }
-     public IConfiguration Configuration { get; }
     public void ConfigureServices(IServiceCollection services) {
         var connectionString = Configuration.GetConnectionString("DefaultConnection");
         services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
@@ -18,7 +19,6 @@ public class Startup {
     public void Configure(WebApplication app, IWebHostEnvironment env) {
         if (!app.Environment.IsDevelopment()) {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
         app.UseHttpsRedirection();
